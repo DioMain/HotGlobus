3c9476f6860772cde0c11a8c65013c94bc993446
DioMain <dima123.zample@gmail.com>
2025-07-31 23:42:20 +0300

    fix: SimpleManagerBase

---
+++ Assets/HotGlobus.Core/Manager/SimpleManagerBase.cs
@@ -0,0 +1,11 @@
+﻿using HotGlobus.Core.Container;
+
+namespace HotGlobus.Core.Manager
+{
+    public abstract class SimpleManagerBase : ManagerBase<SimpleContainer>
+    {
+        public SimpleManagerBase(SimpleContainer container) : base(container)
+        {
+        }
+    }
+}
